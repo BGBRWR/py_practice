935a53fdb753826dd4245475d3b2ee0436a758e7
Austin Walker <awalker@crossroadstech.net>
2017-08-23 06:30:27 -0600

    for in loop practice

--- IntroToLists/lists.py
+++ IntroToLists/lists.py
@@ -43,13 +43,30 @@
 # another_even.sort(reverse=True)
 # print(even)
 
-even = [2, 4, 6, 8]
-odd = [1, 3, 5, 7, 9]
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7, 9]
 
-numbers = [even, odd]
+# numbers = [even, odd]
 
-for number_set in numbers:
-    print(number_set)
+# for number_set in numbers:
+#     print(number_set)
 
-    for value in number_set:
-        print(value)
+#     for value in number_set:
+#         print(value)
+
+menu = []
+menu.append(["egg", "spam", "bacon"])
+menu.append(["egg", "sausage", "bacon"])
+menu.append(["egg", "spam"])
+menu.append(["egg", "bacon", "spam"])
+menu.append(["egg", "bacon", "sausage", "spam"])
+menu.append(["spam", "bacon", "sausage", "spam"])
+menu.append(["spam", "egg", "spam", "spam", "bacon", "spam"])
+menu.append(["spam", "egg", "sausage", "spam"])
+
+# print(menu)
+
+for meal in menu:
+    if not "spam" in meal:
+        for igredient in meal:
+            print(igredient)
