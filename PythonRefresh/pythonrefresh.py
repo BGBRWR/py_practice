a9f62e489ed825822265c20b59e641114e9b0c1f
Austin Walker <bgbrwr@gmail.com>
2017-10-31 06:12:43 -0600

    py practice

---
+++ PythonRefresh/pythonrefresh.py
@@ -0,0 +1,93 @@
+# age = 25
+
+# name = 'Austin Walker'
+
+# todayIsCold = False
+# # print('Hello my name is {0} and I am {1} years old.'.format(name, age))
+
+# # if age > 18:
+# #     print('You are older than 18')
+# # else:
+# #     print('You are 18 or younger')
+
+
+# def hello(name='HUMAN', age='SEVERAL'):
+#     # print('hello world!')
+#     # print('hello mother!')
+#     # print('hello father!')
+#     # print('hello ' + name + '! You are ' + str(age) + ' years old!')
+#     print('Hello {}! You are {} years old!'.format(name, age))
+
+
+# hello(name, age)
+# hello()
+
+
+# dognames = ['Fido', 'Little Sean', 'Big Sean', 'Marky Mark']
+
+# # dognames.insert(0, 'Jane')
+# # del(dognames[2])
+# dognames[1] = 2
+# # print(len(dognames))
+# print(dognames)
+
+# for dog in dognames:
+#     print(dog)
+
+# for x in range(1, 10):
+#     print(x)
+
+# age = 0
+# while age < 18:
+#     print(age)
+#     age += 1
+
+# dogs = {'Fido': 8, 'Tank': 5, 'Puppy': 2}
+
+# dogs['Sarah'] = 6
+
+# print(dogs['Puppy'])
+
+# words = ["PoGo", "Spange", "Lie-Fi"]
+# definitions = ["Slang for Pokemon Go", "To collect spare change, either from couches, passerbys on the street or any numerous other ways and means",
+#                "When your phone or tablet indicates that you are connected to a wireless network, however you are still unable to load webpages or use any internet services with your device"]
+# cooldictionary = {}
+# for x in range(0, len(words)):
+#     cooldictionary[words[x]] = definitions[x]
+
+# print(cooldictionary)
+
+# class Dog:
+
+#     def __init__(self, name, age, furcolor):
+#         self.name = name
+#         self.age = age
+#         self.furcolor = furcolor
+
+#     dogInfo = 'Hey dogs are cool'
+
+#     def bark(self):
+#         print('BARK!')
+
+
+# mydog = Dog('Fido', 13, 'Brown')
+# mydog.bark()
+# # mydog.name = 'Fido'
+# # mydog.age = 16
+# print(mydog.name)
+# print(mydog.age)
+# print(mydog.furcolor)
+
+# Dog.dogInfo = 'Hey There'
+# print(Dog.dogInfo)
+
+# class Car:
+#     def __init__(self, year, make, model):
+#         self.year = year
+#         self.make = make
+#         self.model = model
+
+#     def age(self):
+#         print(2017 - self.year)
+
+# Car.age(2015)
